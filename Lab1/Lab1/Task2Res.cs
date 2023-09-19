db3911bf3908caafcb654f0922128256571bc8a2
ankey18 <anna2002f@yandex.ru>
2023-09-19 23:33:28 +0300

    task2 some changes

--- Lab1/Lab1/Task2Res.cs
+++ Lab1/Lab1/Task2Res.cs
@@ -18,25 +18,32 @@ namespace Lab1
             InitializeComponent();
         }
 
-        private void CreateHist(int[] r, int[] g, int[] b, Chart chart)
+        private void CreateHist(int[] ii, Chart chart)
         {
             chart.Series.Clear();
-
-            Series redSeries = new Series("Red");
-            Series greenSeries = new Series("Green");
-            Series blueSeries = new Series("Blue");
-
+            Series series = new Series(); 
             for (int i = 0; i < 256; i++)
             {
-                redSeries.Points.AddXY(i, r[i]);
-                greenSeries.Points.AddXY(i, g[i]);
-                blueSeries.Points.AddXY(i, b[i]);
+                if (chart.Name == "chartRed")
+                {
+                    series.Color = Color.Red;
+                    series.Points.AddXY(i, ii[i]);
+                }
+                else if (chart.Name == "chartGreen")
+                {
+                    series.Color = Color.Green;
+                    series.Points.AddXY(i, ii[i]);
+                }
+                else if (chart.Name =="chartBlue")
+                {
+                    series.Color = Color.Blue;
+                    series.Points.AddXY(i, ii[i]);
+                }
             }
-
-            chart.Series.Add(redSeries);
-            chart.Series.Add(greenSeries);
-            chart.Series.Add(blueSeries);
-
+              
+                                 
+            chart.Series.Add(series);
+          
             chart.ChartAreas[0].AxisX.Minimum = 0;
             chart.ChartAreas[0].AxisX.Maximum = 255;
             chart.ChartAreas[0].AxisY.Minimum = 0;
@@ -51,17 +58,16 @@ namespace Lab1
             int[] greenHist1 = new int[256];
             int[] blueHist1 = new int[256];
 
-            int[] redHist2 = new int[256];
-            int[] greenHist2 = new int[256];
-            int[] blueHist2 = new int[256];
+            //int[] redHist2 = new int[256];
+            //int[] greenHist2 = new int[256];
+            //int[] blueHist2 = new int[256];
 
-            int[] redHist3 = new int[256];
-            int[] greenHist3 = new int[256];
-            int[] blueHist3 = new int[256];
+            //int[] redHist3 = new int[256];
+            //int[] greenHist3 = new int[256];
+            //int[] blueHist3 = new int[256];
 
             try
-            {
-                
+            { 
                 Bitmap img1 = new Bitmap(pictureBox2.Image);
                 Bitmap img2 = new Bitmap(pictureBox2.Image);
                 Bitmap img3 = new Bitmap(pictureBox2.Image);
@@ -71,48 +77,23 @@ namespace Lab1
                     {
                         Color c = img1.GetPixel(i, j);
                         Color nc = Color.FromArgb(c.A, c.R, 0, 0);
-                        img1.SetPixel(i, j, nc);
-                        c = img1.GetPixel(i, j);
-                        redHist1[c.R]++;
-                        greenHist1[c.G]++;
-                        blueHist1[c.B]++;
-                    }
-                }
-                pixelRed.Image = img1;
-                CreateHist(redHist1, greenHist1, blueHist1, chartRed);
-
-                for (int i = 0; i < img2.Width; i++)
-                {
-                    for (int j = 0; j < img2.Height; j++)
-                    {
-                        Color c = img2.GetPixel(i, j);
-                        Color nc = Color.FromArgb(c.A, 0, c.G, 0);
+                        img1.SetPixel(i, j, nc); 
+                        nc = Color.FromArgb(c.A, 0, c.G, 0);
                         img2.SetPixel(i, j, nc);
-                        c = img2.GetPixel(i, j);
-                        redHist2[c.R]++;
-                        greenHist2[c.G]++;
-                        blueHist2[c.B]++;
-                    }
-                }
-                pixelGreen.Image = img2;
-                CreateHist(redHist2, greenHist2, blueHist2, chartGreen);
-
-                for (int i = 0; i < img3.Width; i++)
-                {
-                    for (int j = 0; j < img3.Height; j++)
-                    {
-                        Color c = img3.GetPixel(i, j);
-                        Color nc = Color.FromArgb(c.A, 0, 0, c.B);
+                        nc = Color.FromArgb(c.A, 0, 0, c.B);
                         img3.SetPixel(i, j, nc);
-                        c = img3.GetPixel(i, j);
-                        redHist3[c.R]++;
-                        greenHist3[c.G]++;
-                        blueHist3[c.B]++;
+
+                        redHist1[img1.GetPixel(i,j).R]++;
+                        greenHist1[img2.GetPixel(i, j).G]++;
+                        blueHist1[img3.GetPixel(i, j).B]++;
                     }
                 }
-
+                pixelRed.Image = img1;  
+                pixelGreen.Image = img2;
                 pixelBlue.Image = img3;
-                CreateHist(redHist3, greenHist3, blueHist3, chartBlue);
+                CreateHist(redHist1, chartRed);
+                CreateHist(greenHist1, chartGreen);
+                CreateHist(blueHist1, chartBlue);
 
             }
             catch (Exception ex)
