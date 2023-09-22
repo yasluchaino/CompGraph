6573ad708bae8580c7ade50f150789dc6148aacc
ankey18 <anna2002f@yandex.ru>
2023-09-22 20:26:25 +0300

    fix

--- Lab1/Lab1/Form1.cs
+++ Lab1/Lab1/Form1.cs
@@ -19,11 +19,7 @@ namespace Lab1
 
         private void task2_Click(object sender, EventArgs e)
         {
-<<<<<<< Updated upstream
  Task2Res task2 = new Task2Res();
-=======
-            Task2 task2 = new Task2();
->>>>>>> Stashed changes
             // Show the settings form
             task2.Show();
         }
