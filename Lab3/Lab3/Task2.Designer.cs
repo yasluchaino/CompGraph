295c39a26c9dafda84003c5c8fdadbfa86fc415e
afurmanova18 <afurmanova18@mail.ru>
2023-09-29 00:09:04 +0300

    bresenham

--- Lab3/Lab3/Task2.Designer.cs
+++ Lab3/Lab3/Task2.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(285, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(828, 525);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 88);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(173, 20);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "алгоритм Брезенхема";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 130);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(111, 20);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "алгоритм ВУ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Task2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1151, 549);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Task2";
             this.Text = "Task2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
     }
 }
--- Lab3/Lab3/Task2.cs
+++ Lab3/Lab3/Task2.cs
@@ -10,11 +10,151 @@ using System.Windows.Forms;
 
 namespace Lab3
 {
+
     public partial class Task2 : Form
     {
+        Bitmap bitmap;
+        Graphics g;
+        private int startX, startY;
+        private bool isDrawing = false;
+
         public Task2()
         {
             InitializeComponent();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bitmap);
+
+            // Добавляем обработчики событий для PictureBox
+            pictureBox1.MouseDown += PictureBox_MouseDown;
+            pictureBox1.MouseMove += PictureBox_MouseMove;
+            pictureBox1.MouseUp += PictureBox_MouseUp;
         }
+
+ 
+
+        private void PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            isDrawing = true;
+         
+            startX = e.X;
+            startY = e.Y;
+
+
+        }
+
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        { 
+            if (isDrawing)
+            {
+                g.Clear(Color.White);
+                if (radioButton1.Checked)
+                {       
+                    Bresenham(startX, startY, e.X, e.Y);                
+                }
+                else
+                {
+                    MessageBox.Show("Please, select a method.");
+                }
+            }
+        }
+
+        private void PictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;         
+        }
+
+        private void plotLineLow(int x0, int y0, int x1, int y1)
+        {
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+            int yi = 1;
+            if (dy < 0)
+            {
+                yi = -1;
+                dy = -dy;
+            }
+            int D = (2 * dy) - dx;
+            int y = y0;
+
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                for (int x = x0; x < x1; x++)
+                {
+                    g.FillRectangle(Brushes.Black,x, y, 1, 1);
+                    if (D > 0)
+                    {
+                        y = y + yi;
+                        D = D + (2 * (dy - dx));
+                    }
+                    else
+                        D = D + 2 * dy;
+                }
+            }
+        }
+
+
+
+        private void plotLineHigh(int x0, int y0, int x1, int y1)
+        {
+            int dx = x1 - x0;
+            int dy = y1 - y0;
+            int xi = 1;
+            if (dx < 0)
+            {
+                xi = -1;
+                dx = -dx;
+            }
+            int D = (2 * dx) - dy;
+            int x = x0;
+
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                for (int y = y0; y < y1; y++)
+                {
+                    g.FillRectangle(Brushes.Black, x, y, 1, 1);
+                    if (D > 0)
+                    {
+                        x = x + xi;
+                        D = D + (2 * (dx - dy));
+                    }
+                    else
+                        D = D + 2 * dx;
+                }
+            }
+
+        }
+
+
+
+        void Bresenham(int x0, int y0, int x1, int y1)
+        {
+            if( Math.Abs(y1-y0) < Math.Abs(x1 - x0))
+            {
+                if (x0 > x1)
+                {
+                    plotLineLow(x1,y1,x0,y0);
+                }
+                else
+                {
+                    plotLineLow(x0,y0,x1,y1);
+                }
+            }
+            else
+            {
+                if (y0 > y1) {
+                    plotLineHigh(x1,y1,x0,y0);
+                }
+                else
+                {
+                    plotLineHigh(x0,y0,x1,y1);
+                }
+            }
+
+            pictureBox1.Image = bitmap; 
+
+        }
+
+
     }
 }
