e7f6b36246d5c82683ca3a69ae2cc3c9eae659f6
afurmanova18 <afurmanova18@mail.ru>
2023-10-20 23:13:50 +0300

    заготовка 5 лаба

---
+++ lab5/lab5/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Task3 task3 = new Task3();
+            task3.Show();
+        }
+    }
+}
