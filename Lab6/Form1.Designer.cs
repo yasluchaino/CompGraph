2531d02f94d56e154be5ed3d237f9ccdf8366e75
afurmanova18 <afurmanova18@mail.ru>
2023-10-28 18:49:48 +0300

    start lab6

--- Lab6/Form1.Designer.cs
+++ Lab6/Form1.Designer.cs
@@ -28,21 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(810, 578);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Тетраэдр";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 47);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(90, 20);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Гексаэдр";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 73);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(85, 20);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Октаэдр";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Location = new System.Drawing.Point(828, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(122, 106);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(962, 602);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Lab6.Form1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
 
 namespace Lab6
 {
@@ -62,13 +64,235 @@ namespace Lab6
                 Faces = faces;
             }
         }
+        public class Tetrahedron
+        {
+            public Point3D[] Vertices { get; set; }
+            public List<Edge> Edges { get; set; }
+
+            public Tetrahedron()
+            {
+                // Задайте координаты вершин тетраэдра
+                Vertices = new Point3D[4];
+                Vertices[0] = new Point3D(0, 0, 0);
+                Vertices[1] = new Point3D(1, 0, 0);
+                Vertices[2] = new Point3D(0.5, Math.Sqrt(3) / 2, 0);
+                Vertices[3] = new Point3D(0.5, Math.Sqrt(3) / 6, Math.Sqrt(6) / 3);
+
+                // Создайте рёбра тетраэдра
+                Edges = new List<Edge>();
+                Edges.Add(new Edge(Vertices[0], Vertices[1]));
+                Edges.Add(new Edge(Vertices[0], Vertices[2]));
+                Edges.Add(new Edge(Vertices[0], Vertices[3]));
+                Edges.Add(new Edge(Vertices[1], Vertices[2]));
+                Edges.Add(new Edge(Vertices[1], Vertices[3]));
+                Edges.Add(new Edge(Vertices[2], Vertices[3]));
+            }
+        }
+        public class Hexahedron
+        {
+            public Point3D[] Vertices { get; set; }
+            public List<Edge> Edges { get; set; }
+
+            public Hexahedron()
+            {
+                Vertices = new Point3D[8];
+
+                // Задайте координаты вершин гексаэдра
+                Vertices[0] = new Point3D(0, 0, 0);
+                Vertices[1] = new Point3D(1, 0, 0);
+                Vertices[2] = new Point3D(1, 1, 0);
+                Vertices[3] = new Point3D(0, 1, 0);
+                Vertices[4] = new Point3D(0, 0, 1);
+                Vertices[5] = new Point3D(1, 0, 1);
+                Vertices[6] = new Point3D(1, 1, 1);
+                Vertices[7] = new Point3D(0, 1, 1);
+
+                Edges = new List<Edge>();
+
+                // Создайте рёбра гексаэдра, соединяющие вершины
+                Edges.Add(new Edge(Vertices[0], Vertices[1]));
+                Edges.Add(new Edge(Vertices[1], Vertices[2]));
+                Edges.Add(new Edge(Vertices[2], Vertices[3]));
+                Edges.Add(new Edge(Vertices[3], Vertices[0]));
+
+                Edges.Add(new Edge(Vertices[4], Vertices[5]));
+                Edges.Add(new Edge(Vertices[5], Vertices[6]));
+                Edges.Add(new Edge(Vertices[6], Vertices[7]));
+                Edges.Add(new Edge(Vertices[7], Vertices[4]));
+
+                Edges.Add(new Edge(Vertices[0], Vertices[4]));
+                Edges.Add(new Edge(Vertices[1], Vertices[5]));
+                Edges.Add(new Edge(Vertices[2], Vertices[6]));
+                Edges.Add(new Edge(Vertices[3], Vertices[7]));
+            }
+        }
+        private Tetrahedron tetrahedron;
+        private bool drawTetrahedron = false;
+        private Hexahedron hexahedron;
+        private bool drawHexahedron = false;
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (drawTetrahedron)
+            {
+                DrawTetrahedron(e.Graphics, tetrahedron);
+            }
+            if (checkBox2.Checked)
+            {
+                DrawHexahedron(e.Graphics, hexahedron);
+            }
+        }
+
+        private void DrawTetrahedron(Graphics g, Tetrahedron tetrahedron)
+        {
+            g.Clear(Color.White);
+            Pen pen = new Pen(Color.Black);
+
+            // Масштабирование и смещение для отображения на форме
+            float scale = 100;
+            float offsetX = pictureBox1.Width / 2;
+            float offsetY = pictureBox1.Height / 2;
+            float perspective = 100.0f; // Расстояние от камеры до экрана
+            foreach (Edge edge in tetrahedron.Edges)
+            {
+                // Применение перспективной проекции к каждой вершине
+                Point3D start = PerspectiveProjection(edge.Start, perspective);
+                Point3D end = PerspectiveProjection(edge.End, perspective);
+                Point startPoint = new Point((int)(edge.Start.X * scale + offsetX), (int)(edge.Start.Y * scale + offsetY));
+                Point endPoint = new Point((int)(edge.End.X * scale + offsetX), (int)(edge.End.Y * scale + offsetY));
+                g.DrawLine(pen, startPoint, endPoint);
+            }
+        }
+        private void DrawHexahedron(Graphics g, Hexahedron hexahedronm)
+        {
+            g.Clear(Color.White);
+            Pen pen = new Pen(Color.Black);
+
+            // Масштабирование и смещение для отображения на форме
+            float scale = 100;
+            float offsetX = pictureBox1.Width / 2;
+            float offsetY = pictureBox1.Height / 2;
+            float perspective = 100.0f; // Расстояние от камеры до экрана
+            foreach (Edge edge in hexahedron.Edges)
+            {
+                // Применение аксонометрической проекции к каждой вершине
+                Point3D start = axonometricProjection.Project(edge.Start);
+                Point3D end = axonometricProjection.Project(edge.End);
+                pictureBox1.Invalidate();
+                Point startPoint = new Point((int)(edge.Start.X * scale + offsetX), (int)(edge.Start.Y * scale + offsetY));
+                Point endPoint = new Point((int)(edge.End.X * scale + offsetX), (int)(edge.End.Y * scale + offsetY));
+                g.DrawLine(pen, startPoint, endPoint);
+            }
+        }
+        private void CheckBoxInGroup_CheckedChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.CheckBox clickedCheckBox = sender as System.Windows.Forms.CheckBox;
+
+            // Отключите все остальные чекбоксы в группе
+            foreach (Control control in groupBox1.Controls)
+            {
+                if (control is System.Windows.Forms.CheckBox && control != clickedCheckBox)
+                {
+                    System.Windows.Forms.CheckBox otherCheckBox = control as System.Windows.Forms.CheckBox;
+                    otherCheckBox.Checked = false;
+                }
+            }
+
+            // В зависимости от выбранного чекбокса, выполните соответствующие действия
+            if (clickedCheckBox.Checked)
+            {
+                if (clickedCheckBox == checkBox1)
+                {
+                    drawTetrahedron = true;
+                    drawHexahedron = false;
+                }
+                else if (clickedCheckBox == checkBox2)
+                {
+                    drawHexahedron = true;
+                    drawTetrahedron = false;
+                }
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private AxonometricProjection axonometricProjection;
+
 
         public Form1()
         {
             InitializeComponent();
+            tetrahedron = new Tetrahedron();
+            hexahedron = new Hexahedron();
+            pictureBox1.Paint += new PaintEventHandler(pictureBox1_Paint);
+             foreach (Control control in groupBox1.Controls)
+            {
+                if (control is System.Windows.Forms.CheckBox)
+                {
+                    System.Windows.Forms.CheckBox checkBox = control as System.Windows.Forms.CheckBox;
+                    checkBox.CheckedChanged += CheckBoxInGroup_CheckedChanged;
+                }
+            }
+            axonometricProjection = new AxonometricProjection(Math.PI / 8, Math.PI / 4); // Пример углов
+      
+
+        }
+
+        private Point3D PerspectiveProjection(Point3D point, float perspective)
+        {
+            // Применение перспективной проекции к точке
+            double projectedX = point.X / (1 + point.Z / perspective);
+            double projectedY = point.Y / (1 + point.Z / perspective);
+
+            return new Point3D(projectedX, projectedY, point.Z);
+        }
+        public class AxonometricProjection
+        {
+            // Углы аксонометрической проекции (в радианах)
+            private double alpha; // Угол наклона к оси X
+            private double beta;  // Угол наклона к оси Y
+
+            // Матрица проекции
+            private double[,] projectionMatrix;
+
+            public AxonometricProjection(double alpha, double beta)
+            {
+                this.alpha = alpha;
+                this.beta = beta;
+
+                // Создаем матрицу проекции
+                projectionMatrix = new double[3, 3];
+                projectionMatrix[0, 0] = Math.Cos(beta);
+                projectionMatrix[0, 1] = -Math.Sin(alpha) * Math.Sin(beta);
+                projectionMatrix[0, 2] = 0;
+                projectionMatrix[1, 0] = 0;
+                projectionMatrix[1, 1] = Math.Cos(alpha);
+                projectionMatrix[1, 2] = 0;
+                projectionMatrix[2, 0] = Math.Sin(beta);
+                projectionMatrix[2, 1] = Math.Sin(alpha) * Math.Cos(beta);
+                projectionMatrix[2, 2] = 1;
+            }
+
+            public Point3D Project(Point3D point)
+            {
+                // Применяем матрицу проекции к точке
+                double x = point.X * projectionMatrix[0, 0] + point.Y * projectionMatrix[0, 1] + point.Z * projectionMatrix[0, 2];
+                double y = point.X * projectionMatrix[1, 0] + point.Y * projectionMatrix[1, 1] + point.Z * projectionMatrix[1, 2];
+                double z = point.X * projectionMatrix[2, 0] + point.Y * projectionMatrix[2, 1] + point.Z * projectionMatrix[2, 2];
+
+                return new Point3D(x, y, z);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
 
         }
