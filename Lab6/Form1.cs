0c7cc71497b4855eba7d7713ef41e3352b5ef84d
ankey18 <anna2002f@yandex.ru>
2023-10-28 16:47:35 +0300

    osnovnye classy

---
+++ Lab6/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+
+        // Класс для точки
+        public class Point3D
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Z { get; set; }
+
+            public Point3D(double x, double y, double z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+        }
+
+        // Класс для прямой
+        public class Edge
+        {
+            public Point3D Start { get; set; }
+            public Point3D End { get; set; }
+
+            public Edge(Point3D start, Point3D end)
+            {
+                Start = start;
+                End = end;
+            }
+        }
+
+        // Класс для многоугольника (грани)
+        public class Polygon
+        {
+            public List<Point3D> Vertices { get; set; }
+
+            public Polygon(List<Point3D> vertices)
+            {
+                Vertices = vertices;
+            }
+        }
+
+        // Класс для многогранника
+        public class Polyhedron
+        {
+            public List<Polygon> Faces { get; set; }
+
+            public Polyhedron(List<Polygon> faces)
+            {
+                Faces = faces;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
