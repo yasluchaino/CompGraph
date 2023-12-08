c4905c44c377f071174ec8276fed2c086eb9e525
MyPleasure <kir_zinn@mail.ru>
2023-12-08 20:40:31 +0300

    12lab_3task

---
+++ Lab12/Lab12/task3.cpp
@@ -0,0 +1,332 @@
+﻿#include <GL/glew.h>
+#include <SFML/OpenGL.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// ID шейдерной программы
+GLuint Program;
+// ID атрибута вершин
+GLint Attrib_vertex;
+// ID атрибута цвета
+GLint Attrib_color;
+// ID атрибута текстуры
+GLint Attrib_texture;
+
+// ID VBO вершин
+GLuint VBO;
+
+sf::Image img1,img2;
+GLuint texture1;
+
+GLuint texture2;
+
+GLint Unif_Coef;
+
+float coef = 0.04f;
+
+struct Vertex {
+	GLfloat x;
+	GLfloat y;
+	GLfloat z;
+};
+
+// Исходный код вершинного шейдера
+const char* VertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 coord_pos;
+layout (location = 1) in vec3 color_value;
+layout (location = 2) in vec2 tex_coord_in;
+out vec3 frag_color;
+out vec2 coord_tex;    
+void main() 
+{ 
+    vec3 position = coord_pos * mat3(
+           1, 0, 0,
+            0, cos(1), -sin(1),
+            0, sin(1), cos(1)
+        ) * mat3(
+            cos(-1), 0, sin(-1),
+            0, 1, 0,
+            -sin(-1), 0, cos(-1)
+		);
+	gl_Position = vec4(position, 1.0);
+    frag_color = color_value;
+    coord_tex = tex_coord_in;
+}
+)";
+
+// Исходный код фрагментного шейдера
+const char* FragShaderSource = R"(
+#version 330 core
+in vec3 frag_color;
+in vec2 coord_tex;
+out vec4 color;
+uniform float coef;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+void main() 
+{
+    color = mix(texture(texture1, coord_tex), texture(texture2, coord_tex), coef);
+}
+)";
+
+void ShaderLog(unsigned int shader)
+{
+	int infologLen = 0;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infologLen);
+	if (infologLen > 1)
+	{
+		int charsWritten = 0;
+		std::vector<char> infoLog(infologLen);
+		glGetShaderInfoLog(shader, infologLen, &charsWritten, infoLog.data());
+		std::cout << "InfoLog: " << infoLog.data() << std::endl;
+	}
+}
+
+void checkOpenGLerror() {
+	GLenum errCode;
+	if ((errCode = glGetError()) != GL_NO_ERROR)
+		std::cout << "OpenGl error!: " << errCode << std::endl;
+}
+
+
+
+void InitTextures()
+{
+
+	if (!img1.loadFromFile("4.jpg") || !img2.loadFromFile("5.jpg"))
+	{
+		//return false;
+	}
+
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img1.getSize().x, img1.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, img1.getPixelsPtr());
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img2.getSize().x, img2.getSize().y, 0, GL_RGBA, GL_UNSIGNED_BYTE, img2.getPixelsPtr());
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void InitVBO() {
+
+	glGenBuffers(1, &VBO);
+
+	float cube[] = {
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 0.0f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,   1.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f
+	};
+
+
+	// Передаем вершины в буфер
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
+	checkOpenGLerror();
+}
+
+void InitShader() {
+
+	// Создаем вершинный шейдер
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	// Передаем исходный код
+	glShaderSource(vShader, 1, &VertexShaderSource, NULL);
+	// Компилируем шейдер
+	glCompileShader(vShader);
+	std::cout << "vertex shader \n";
+	ShaderLog(vShader);
+
+
+	// Создаем фрагментный шейдер
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Передаем исходный код
+	glShaderSource(fShader, 1, &FragShaderSource, NULL);
+	// Компилируем шейдер
+	glCompileShader(fShader);
+	std::cout << "fragment shader \n";
+	ShaderLog(fShader);
+
+
+	// Создаем программу и прикрепляем шейдеры к ней
+	Program = glCreateProgram();
+	glAttachShader(Program, vShader);
+	glAttachShader(Program, fShader);
+
+	// Линкуем шейдерную программу
+	glLinkProgram(Program);
+	// Проверяем статус сборки
+	int link_ok;
+	glGetProgramiv(Program, GL_LINK_STATUS, &link_ok);
+	if (!link_ok)
+	{
+		std::cout << "error attach shaders \n";
+		return;
+	}
+
+	// Вытягиваем ID юниформ
+	const char* unif_name = "coef";
+	Unif_Coef = glGetUniformLocation(Program, unif_name);
+	if (Unif_Coef < 0 || Unif_Coef > 1)
+	{
+		std::cout << "could not bind uniform" << std::endl;
+		return;
+	}
+
+	checkOpenGLerror();
+}
+
+void Init() {
+	InitShader();
+	InitTextures();
+	InitVBO();
+	glEnable(GL_DEPTH_TEST);
+}
+
+
+void Draw() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Устанавливаем шейдерную программу текущей
+	glUseProgram(Program);
+
+	glUniform1f(glGetUniformLocation(Program, "coef"), coef);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glUniform1i(glGetUniformLocation(Program, "texture1"), 0);
+
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glUniform1i(glGetUniformLocation(Program, "texture2"), 1);
+
+	// подключаем VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	// Подключаем массив аттрибутов с указанием на каких местах кто находится
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);  // Отключаем VBO
+
+	//Рисуем 
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	// Отключаем массивы атрибутов
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
+
+	glUseProgram(0); // Отключаем шейдерную программу
+}
+
+
+// Освобождение шейдеров
+void ReleaseShader() {
+	// Отключаем шейдерную программу
+	glUseProgram(0);
+	// Удаляем шейдерную программу
+	glDeleteProgram(Program);
+}
+
+// Освобождение буфера
+void ReleaseVBO() {
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glDeleteBuffers(1, &VBO);
+}
+
+void Release() {
+	ReleaseShader();
+	ReleaseVBO();
+}
+
+int main() {
+	sf::Window window(sf::VideoMode(800, 800), "My OpenGL window", sf::Style::Default, sf::ContextSettings(24));
+	window.setVerticalSyncEnabled(true);
+	window.setActive(true);
+	glewInit();
+	Init();
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) { window.close(); }
+			else if (event.type == sf::Event::Resized) { glViewport(0, 0, event.size.width, event.size.height); }
+			else if (event.type == sf::Event::KeyPressed) {
+				switch (event.key.code) {
+				case (sf::Keyboard::W): coef += 0.1f; break;
+				case (sf::Keyboard::S): coef -= 0.1f; break;
+				default: break;
+				}
+				if (coef >= 1.0f)
+					coef = 1.0f;
+				if (coef <= 0.0f)
+					coef = 0.0f;
+			}
+		}
+
+		Draw();
+		window.display();
+	}
+	Release();
+	return 0;
+}
